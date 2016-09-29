7e6899aa718075c6564bb570c5ebdf5cc96d9a7d
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-09-29 21:55:26 +0300

    Implemented login_required decorator & token_based_auth middleware for token-based authorization

--- chatty/auth/decorators.py
+++ chatty/auth/decorators.py
@@ -2,4 +2,9 @@ from functools import wraps
 
 
 def login_required(handler):
-    pass
+    '''
+    This function adds 'login_required' property to provided 'handler',
+    which indicates that only authorized user can access handler.
+    '''
+    handler.login_required = True
+    return handler
--- chatty/auth/middlewares.py
+++ chatty/auth/middlewares.py
@@ -1,7 +1,21 @@
 from aiohttp import web
 
+from chatty.settings import TOKEN_KEY
+
 
 async def token_based_auth_middleware(app, handler):
+    '''
+    This middleware will check user access token if handler requires auth.
+    '''
     async def middleware(request):
-        pass
+        login_required = getattr(handler, 'login_required', False)
+        if login_required:
+            token = request.headers.get('Authorization', None)
+            if token:
+                is_valid_token = await app.redis.get(TOKEN_KEY.format(uuid=token))
+                if is_valid_token:
+                    return await handler(request)
+            return web.HTTPUnauthorized()
+        else:
+            return await handler(request)
     return middleware
