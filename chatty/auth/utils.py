172e54091e422da07367e455ff49977b36a7ad24
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-09-30 10:20:46 +0300

    Implemented chatty.auth.is_valid_token function

--- chatty/auth/utils.py
+++ chatty/auth/utils.py
@@ -1,2 +1,11 @@
+from chatty.settings import TOKEN_KEY
+
 async def is_valid_token(redis, token):
-    pass
+    '''
+    This method returns username if provided token is exists,
+    else returns false.
+    '''
+    user = await redis.get(TOKEN_KEY.format(uuid=token))
+    if user:
+        return user.decode('utf-8')
+    return False
