758c221958e2687ab1ce587a28ed757aa05cc1a2
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-09-30 12:58:50 +0300

    Implemented login & register views

--- chatty/auth/utils.py
+++ chatty/auth/utils.py
@@ -1,11 +1,65 @@
-from chatty.settings import TOKEN_KEY
+from hashlib import pbkdf2_hmac
+from binascii import hexlify
+
+from chatty.settings import USER_KEY, TOKEN_KEY, TOKEN_TTL, SECRET_KEY
+
 
 async def is_valid_token(redis, token):
     '''
-    This method returns username if provided token is exists,
+    This function returns username if provided token is exists,
     else returns false.
     '''
     user = await redis.get(TOKEN_KEY.format(uuid=token))
     if user:
         return user.decode('utf-8')
     return False
+
+async def is_user_exists(redis, username):
+    '''
+    Checks that USER_KEY with given username exists.
+    '''
+    exists = await redis.exists(USER_KEY.format(name=username))
+    return exists
+
+def create_hash(password):
+    '''
+    Creates pbkdf2 hash using provided password and SECRET_KEY defined at settings
+    '''
+    if not isinstance(password, bytes):
+        password = password.encode('utf-8')
+    dk = pbkdf2_hmac('sha256', password, SECRET_KEY, 100000)
+    return hexlify(dk).decode('utf-8')
+
+async def delete_token(redis, token):
+    '''
+    Deletes user access token when user logouts
+    '''
+    token_key = TOKEN_KEY.format(uuid=token)
+    await redis.delete(token_key)
+
+async def create_token(redis, username):
+    '''
+    Returns token that user must use in authorization.
+    '''
+    token = create_hash(username)
+    token_key = TOKEN_KEY.format(uuid=token)
+    await redis.set(token_key, username)
+    await redis.expire(token_key, TOKEN_TTL)
+    return token
+
+async def create_user(redis, username, password):
+    '''
+    Actually, just stores password hash at formatted USER_KEY.
+    '''
+    password = create_hash(password)
+    await redis.set(USER_KEY.format(name=username), password)
+    token = await create_token(redis, username)
+    return token
+
+async def authorize(redis, username, password):
+    '''
+    Checks that provided password of user matches password hash stored at redis.
+    '''
+    true_password_hash = await redis.get(USER_KEY.format(name=username))
+    provided_password_hash = create_hash(password).encode('utf-8')
+    return true_password_hash == provided_password_hash
--- chatty/auth/views.py
+++ chatty/auth/views.py
@@ -1,21 +1,59 @@
 from aiohttp import web
+from aiohttp_jinja2 import template
+
+from chatty.auth.decorators import login_required, anonymous_required
+from chatty.auth.utils import is_user_exists, create_user, authorize, create_token
+from chatty.auth.forms import RegisterForm, LoginForm
 
 
+@anonymous_required
+@template('register.html')
 class RegisterView(web.View):
 
-    def get(self):
-        pass
+    async def get(self):
+        form = RegisterForm()
+        return {
+            'form': form,
+        }
 
-    def post(self):
-        pass
+    async def post(self):
+        form = RegisterForm(await self.request.post())
+        if form.validate():
+            if not (await is_user_exists(self.request.app.redis, form.username.data)):
+                token = await create_user(self.request.app.redis, form.username.data, form.password.data)
+                return {
+                    'token': token,
+                }
+            else:
+                form.username.errors.append('User already exists')
+        return {
+            'form': form,
+        }
 
+@anonymous_required
+@template('login.html')
 class LoginView(web.View):
 
-    def get(self):
-        pass
+    async def get(self):
+        form = LoginForm()
+        return {
+            'form': form,
+        }
 
-    def post(self):
-        pass
+    async def post(self):
+        form = LoginForm(await self.request.post())
+        if form.validate():
+            if (await authorize(self.request.app.redis, form.username.data, form.password.data)):
+                token = await create_token(self.request.app.redis, form.username.data)
+                return {
+                    'token': token,
+                }
+            else:
+                form.username.errors.append('Invalid username or password')
+        return {
+            'form': form,
+        }
+
 
 class LogoutView(web.View):
 
